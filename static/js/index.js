a257a4aeb50e929cf753f00ad7d64eaef702fccd
Omri Avrahami <omri.avrahami@gmail.com>
2022-11-22 19:11:46 +0200

    Initial commit.

---
+++ static/js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $('.results-carousel').slick({
+    dots: false,
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       arrows: true,
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     }
+    //   }
+    // ]
+  });
+})
